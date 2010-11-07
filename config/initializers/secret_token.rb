c2ed1147af5ee327b53493bffe32cc44bffaf255
Vasily Reys <reys.vasily@gmail.com>
2010-11-07 17:44:02 +0600

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hdtv::Application.config.secret_token = 'b16a29e04555cca45885ecfbf3c1088c54634829dff630b7d6bb0f2c1cf4f0c791bc43c0ca85f1b9d7e3509c2d46192c67af97baad539d07307bd1d05b795c57'
