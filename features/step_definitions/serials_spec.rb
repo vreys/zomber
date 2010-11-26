dbd54d815b32d04ad7dba0cdc76746a0a517de09
mimikadze <mimikadze@Nebuchadnezzar.(none)>
2010-11-26 19:59:12 +0600

    serials page specification added

--- features/step_definitions/serials_spec.rb
+++ features/step_definitions/serials_spec.rb
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
 Given /^есть (\d+) разных сериалов$/ do |count_serials|
+  count_serials.to_i.times { make_serial }
 
+  Repository.index!
+end
+
+When /^я захожу в раздел сериалов$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^я должен увидеть (\d+) названий сериалов с иконками$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
 end
