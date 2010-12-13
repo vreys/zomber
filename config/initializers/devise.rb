c57f73652f6c1b0782b59a48fb098790370ef928
Vasily Reys <reys.vasily@gmail.com>
2010-12-13 20:08:49 +0600

    Поменял email адрес от которого происходит рассылка Devise уведомлений

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "no-reply@localhost"
+  config.mailer_sender = "noreply@zomber.tv"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
