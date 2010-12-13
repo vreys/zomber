e8a87cc690862cef60a285ac070ad3e7e89649c7
Vasily Reys <reys.vasily@gmail.com>
2010-12-13 16:08:39 +0600

    Привел в форму авторизации в более приличный вид

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def render_logo
+    render :partial => 'shared/logo'
+  end
+
+  def render_footer
+    render :partial => 'shared/footer'
+  end
+
+  def render_messages
+    render :partial => 'shared/messages'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Hdtv::Application.routes.draw do
   root :to => 'serials#index'
 
-  devise_for :users, :controllers => { :invitations => 'users/invitations' }, :skip => [:sessions, :invitations]
-  
-  as :user do
-    # Registration
-    get 'signup/:invitation_token', :to => "users/invitations#edit",   :as => :accept_user_invitation
-    put 'signup',                   :to => "users/invitations#update", :as => :user_invitation
+  scope :module => "users" do
+    devise_for :users, :controllers => { :invitations => 'invitations' }, :skip => [:sessions, :invitations]
     
-    # Authentication
-    post 'signin',  :to => "devise/sessions#create",  :as => :user_session
-    get  'signin',  :to => "devise/sessions#new",     :as => :new_user_session
-    get  'signout', :to => "devise/sessions#destroy", :as => :destroy_user_session
+    as :user do
+      # Registration
+      get 'signup/:invitation_token', :to => "invitations#edit",   :as => :accept_user_invitation
+      put 'signup',                   :to => "invitations#update", :as => :user_invitation
+      
+      # Authentication
+      post 'signin',  :to => "sessions#create",  :as => :user_session
+      get  'signin',  :to => "sessions#new",     :as => :new_user_session
+      get  'signout', :to => "sessions#destroy", :as => :destroy_user_session
+    end
   end
 
   resources :serials, :only => [:index, :show] do
