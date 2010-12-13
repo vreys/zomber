1707391c05ed671640e1775138e9a832f941bf4c
Vasily Reys <reys.vasily@gmail.com>
2010-12-13 13:11:10 +0600

    Сделал роуты для регистрации и аутентификации более приятными

--- app/controllers/serials_controller.rb
+++ app/controllers/serials_controller.rb
@@ -1,6 +1,8 @@
 class SerialsController < ApplicationController
+  skip_before_filter :authenticate_user!, :only => [ :index ]
+  
   def index
-
+    redirect_to new_user_session_path unless user_signed_in?
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Hdtv::Application.routes.draw do
-  devise_for :users, :controllers => { :invitations => "users/invitations" }
-
   root :to => 'serials#index'
+
+  devise_for :users, :controllers => { :invitations => 'users/invitations' }, :skip => [:sessions, :invitations]
+  
+  as :user do
+    # Registration
+    get 'signup/:invitation_token', :to => "users/invitations#edit",   :as => :accept_user_invitation
+    put 'signup',                   :to => "users/invitations#update", :as => :user_invitation
+    
+    # Authentication
+    post 'signin',  :to => "devise/sessions#create",  :as => :user_session
+    get  'signin',  :to => "devise/sessions#new",     :as => :new_user_session
+    get  'signout', :to => "devise/sessions#destroy", :as => :destroy_user_session
+  end
 
   resources :serials, :only => [:index, :show] do
     match ':season_index/:episode_index' => 'episodes#show', :as => :episode
