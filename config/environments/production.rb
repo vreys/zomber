6d83175c08aadb644392e6f2a00551cd5b0395df
Vasily Reys <reys.vasily@gmail.com>
2010-12-15 15:18:34 +0600

    SMTP конфиг для AcionMailera в продакшене

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,4 +48,9 @@ Hdtv::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.default_url_options = { :host => 'zomber.tv' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.file_settings = {
+    :address => 'smtp.ktnet.kg',
+    :port => 25
+  }
 end
