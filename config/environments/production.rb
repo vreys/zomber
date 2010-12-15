80f2c25b355de00b0a06a6d71835063f9d302fa7
Vasily Reys <reys.vasily@gmail.com>
2010-12-15 17:03:11 +0600

    Фикс для smtp settings в продакшн окружении

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Hdtv::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'zomber.tv' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.file_settings = {
+  config.action_mailer.smtp_settings = {
     :address => 'mail.aknet.kg',
     :port => 25
   }
