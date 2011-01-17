a51e027ebc9606c695b59784c2be0e1a1608055d
Vasily Reys <reys.vasily@gmail.com>
2011-01-17 18:37:01 +0600

    Фикс spec_helper в соответствии с новыми путями до фикстур

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@ require 'remarkable/devise/invitable'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 # Requires factories
-Dir[Rails.root.join("test/factories/*.rb")].each {|f| require f}
+Dir[Rails.root.join("spec/factories/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   config.include Paperclip::Shoulda::Matchers
@@ -30,12 +30,12 @@ RSpec.configure do |config|
   config.mock_with :mocha
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/factories"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # config.use_transactional_fixtures = true
 
   config.after(:each, :type => :model) do
     cleanup_dirs = [REPOS_PATH, POSTERS_PATH, THUMBNAILS_PATH]
